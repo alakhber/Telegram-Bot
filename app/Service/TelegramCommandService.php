7fb5f4b010e44159f087358919f62a1ffbf05ef6
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-11-29 13:27:37 +0400

    debug

--- app/Service/TelegramCommandService.php
+++ app/Service/TelegramCommandService.php
@@ -21,18 +21,17 @@ class TelegramCommandService
         $lastUpdateMessageId = is_null($tg) ? null : $tg->update_id;
 
         $tgMessages = $this->tgBot->getMessages($lastUpdateMessageId);
-        
+
         foreach ($tgMessages as $value) {
             try {
-        
+
                 if (is_null($value['file'])) {
                     $this->tgBot->sendMessage($value['chat_id'], $value['message_id'], 'Qr Düzgün Deyil Zəhmət Olmasa Yenidən Göndərin !');
                 }
-        
+
                 DB::beginTransaction();
-                    Telegram::create($value);
+                Telegram::create($value);
                 DB::commit();
-           
             } catch (\Throwable $th) {
                 DB::rollBack();
             }
@@ -42,38 +41,52 @@ class TelegramCommandService
     public function _readQr()
     {
         $qrs = Telegram::whereIsRead(0)->whereCheckFile(1)->get();
-        
+
         $qrScan = new QrScanner();
-        
+
         foreach ($qrs as $qr) {
             $result = $qrScan->getResult(storage_path('app/public/' . $qr->file));
-        
+
             try {
-        
+
                 DB::beginTransaction();
-        
-                ReadKgo::create(['kgo' => str_replace('KGO9920','',$result)]);
+
+                ReadKgo::create(['kgo' => str_replace('KGO9920', '', $result)]);
                 $qr->update(['is_read' => true]);
-        
+
                 DB::commit();
                 $this->tgBot->sendMessage($qr['chat_id'], $qr['message_id'], 'QR İcraya Göndərildi !');
-        
             } catch (\Throwable $th) {
                 DB::rollBack();
             }
         }
     }
-    
-    public function _updateStatus(){
+
+    public function _updateStatus()
+    {
         $connection = DB::connection('cango');
         $getKgos = ReadKgo::whereIsUpdate(0)->get();
 
         foreach ($getKgos as $key => $value) {
-            $orderId = $connection->table('orders')->where('id',str_replace('KGO9920','',$value->kgo))->first();
-            if(!is_null($orderId)){
-            //    $courierReqPackages = 
-                dump($orderId->id);
+            //  Order Tableden Orderin Tapilmasi
+            $orderId = $connection->table('orders')->where('id', str_replace('KGO9920', '', $value->kgo))->first();
+            if (!is_null($orderId)) {
+                // Courier Request Packages Tableden  Order Id gore Kuryerin Tapilmasi
+                $courierReqPackages = $connection->table('courier_request_packages')->where('package_id', $orderId->package_id)->first();
+                if (!is_null($courierReqPackages)) {
+                    // Courier Request  Tableden  Courier Request Packages tablesindeki courier_request_id  gore Kuryer Requestin Tapilmasi
+                    $courierRequest =  $connection->table('courier_request')->where('id', $courierReqPackages->courier_request_id)->first();
+                    if (!is_null($courierRequest)) {
+                        dump(['courier_request' => $courierRequest->id]);
+                    }
+                    dump(['courier_request_packages' => $courierRequest->id]);
+                }
 
+                // Packages Tableden  Order Id gore paketin Tapilmasi
+                $package = $connection->table('packages')->where('id', $orderId->package_id)->first();
+                if (!is_null($courierReqPackages)) {
+                    dump(['courier_request' => $courierRequest->id]);
+                }
             }
         }
     }
